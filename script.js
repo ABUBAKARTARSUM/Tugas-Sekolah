a8ac2468e0ec2c36453889e7e0d54100f386d6f9
ABUBAKARTARSUM <bakaartarsum@gmail.com>
2021-08-13 11:13:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// style javascript untukfoto about abu bakar
+document.addEventListener('click', function (e) {
+  if (e.target.classList.contains('gallery-item')) {
+    const src = e.target.getAttribute('src');
+    document.querySelector('.modal-img').src = src;
+    const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+    myModal.show();
+  }
+});
